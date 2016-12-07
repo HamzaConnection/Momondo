fcb1cca16553673bd343b2f638756bc4d7ae3b45
Hamza Laroussi <hamzaconnection@hotmail.com>
2016-12-07 12:23:06 +0100

    FlightsInBetween kaldet fra vores js til at virke!

--- src/main/webapp/AirlineJS.js
+++ src/main/webapp/AirlineJS.js
@@ -1,23 +1,28 @@
 var myMomondo = angular.module('myMomondo', []);
 myMomondo.controller('AirlineController', ['$http', '$scope', function ($http, $scope) {
         $scope.title = "HamzaConnection Airways";
-        $scope.destination = "";
-        $scope.origin = "";
-        $scope.date = "";
-        $scope.passengers = document.getElementById("passengers").value;
+        $scope.destination = "STN";
+        $scope.origin = "CPH";
+        $scope.date = "2017-01-03";
+        $scope.passengers = 3;
+        $scope.show = false;
         setTimeout(function () {
             console.log($scope.passengers);
             console.log($scope.date);
             console.log($scope.destination);
             console.log($scope.origin);
-        }, 5000);
+            console.log($scope.show);
+        }, 2000);
         $scope.search = function () {
             $http.get('http://localhost:8080/momondo/api/flights/'+$scope.origin + '/' + $scope.destination + '/' + $scope.date + '/' + $scope.passengers)
                     .success(function (data, status) {
                         $scope.data = data;
+                        
+                        $scope.show = true;
                         console.log("Works!");
-                        console.log(data[0]);
+                        console.log(data.flights);
                         console.log(status);
+                        console.log($scope.show);
                     })
                     .error(function (status) {
                         console.log("nope");
